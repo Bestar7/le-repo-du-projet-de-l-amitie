cadba2faa8fad00c07cb3538c928bc3d0449e202
Joachim ordi IPL <joachim.bastin@student.vinci.be>
2021-11-12 17:46:09 +0100

    add - library DB

--- src/DB.java
+++ src/DB.java
@@ -1,3 +1,12 @@
 public class DB {
-    //hello
+    public static void main(String[] args) {
+        String url="jdbc:postgresql://172.24.2.6:5432/dbjoachimbastin";
+        Connection conn=null;
+        try {
+            conn=DriverManager.getConnection(url,"joachimbastin","IQXR6CLVW");
+        } catch (SQLException e) {
+            System.out.println("Impossible de joindre le server !");
+            System.exit(1);
+        }
+    }
 }
