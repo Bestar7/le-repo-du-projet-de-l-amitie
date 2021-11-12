958e382749c648c87954dbb2ac642ace55948747
Joachim ordi IPL <joachim.bastin@student.vinci.be>
2021-11-12 17:59:52 +0100

    add - library DB ISSOU

--- src/DB.java
+++ src/DB.java
@@ -1,12 +1,47 @@
+import java.sql.*;
+
 public class DB {
+    private static DB instance;
+    private static Connection conn;
+
     public static void main(String[] args) {
-        String url="jdbc:postgresql://172.24.2.6:5432/dbjoachimbastin";
-        Connection conn=null;
+        // chargement à l'execution
         try {
-            conn=DriverManager.getConnection(url,"joachimbastin","IQXR6CLVW");
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver PostgreSQL manquant !");
+            System.exit(1);
+        }
+
+        // création de connection
+        String url = "jdbc:postgresql://172.24.2.6:5432/dbjoachimbastin";
+        try {
+            conn = DriverManager.getConnection(url,"joachimbastin","IQXR6CLVW");
         } catch (SQLException e) {
             System.out.println("Impossible de joindre le server !");
             System.exit(1);
+        }
+    }
+
+    private DB(){
+        main(null);
+    }
+
+    public static DB getInstance() {
+        if (instance == null)
+            instance = new DB();
+        return instance;
+    }
+
+    public static Connection getConnection(){
+        return conn;
+    }
+
+    public void closeConnection(){
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
